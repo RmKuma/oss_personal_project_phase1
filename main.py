3ad81293cc3cab08eea782434e2b1a399cd8839a
RmKuma <party1996115@gmail.com>
2024-06-03 13:12:09 +0900

    Cursor control
    * 키보드를 꾹 누르는 경우 커서 이동 빠르게 진행
    * 커서가 이동시 좌우 최대 최소 이동 제한 설정
    * 조준선 구현

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ PPM = 20.0 / (640 // SCREEN_WIDTH) # pixels per meter
 TIME_STEP = 1.0 / FPS
 GRAVITY = -20
 FRICTION = 0.5
-RESTITUTION = 0.3
+RESTITUTION = 0.4
+
+CURSOR_BOUNDARY = 2
+CURSOR_MIN = 0 + CURSOR_BOUNDARY
+CURSOR_MAX = SCREEN_WIDTH // PPM - CURSOR_BOUNDARY
 
 GROUND_COLOR = [240,128,128, 255]
 
@@ -46,13 +50,6 @@ class ContactListener(b2ContactListener):
             if bodyA.userData["level"] == bodyB.userData["level"]:
                 self.to_destroy.append([bodyA, bodyB])
 
-    # def EndContact(self, contact):
-    #     bodyA = contact.fixtureA.body
-    #     bodyB = contact.fixtureB.body
-    #     self.collisions.remove((bodyA, bodyB))
-    #     print(f"Collision ended between {bodyA} and {bodyB}")
-
-
 class WatermelonGame:
     def __init__(self):
         pygame.init()
@@ -64,7 +61,7 @@ class WatermelonGame:
         self.world.contactListener = self.contact_listener
         self.watermelons = []
         self.before_positions = None
-        self.current_watermelon = None
+        self.cursor = [SCREEN_WIDTH / 2 / PPM, SCREEN_HEIGHT / PPM - 2]
         self.create_ground()
 
     def create_ground(self):
@@ -112,33 +109,55 @@ class WatermelonGame:
             if abs(cur_positions[i].x - self.before_positions[i].x) > 0.7 or  abs(cur_positions[i].y - self.before_positions[i].y) > 0.7:
                 check = False
         return check
+    
+    # 커서가 화면 밖으로 나가는 것을 방지하는 함수
+    def change_cursor_position(self, val):
+        if self.cursor[0] + val < CURSOR_MAX and self.cursor[0] + val > CURSOR_MIN:
+            self.cursor[0] += val
+
+    def draw_dotted_line(self, start_pos, end_pos, color, width=3, dash_length=1 * PPM):
+        x1, y1 = start_pos[0] * PPM, start_pos[1] * PPM
+        x2, y2 = end_pos[0] * PPM, end_pos[1] * PPM
+        
+        distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5 
+        dashes = int(distance / dash_length)
+        
+        for i in range(dashes):
+            print(i)
+            start = (x1 + (x2 - x1) * i / dashes , y1 + (y2 - y1) * i / dashes )
+            end = (x1 + (x2 - x1) * (i + 0.5) / dashes , y1 + (y2 - y1) * (i + 0.5) / dashes )
+            pygame.draw.line(self.screen, color, start, end, width)
 
 
     def run(self):
 
+        # pygame에 box2d 오브젝트들을 draw하는 함수
         def my_draw_polygon(polygon, body, fixture):
             vertices = [(body.transform * v) * PPM for v in polygon.vertices]
             vertices = [(v[0], SCREEN_HEIGHT - v[1]) for v in vertices]
             pygame.draw.polygon(self.screen, GROUND_COLOR, vertices)
-        polygonShape.draw = my_draw_polygon
 
         def my_draw_circle(circle, body, fixture):
             position = body.transform * circle.pos * PPM
             position[1] = SCREEN_HEIGHT - position[1]
             pygame.draw.circle(self.screen, body.userData["color"], [int(x) for x in position], int(circle.radius * PPM))
+            
+        polygonShape.draw = my_draw_polygon
         circleShape.draw = my_draw_circle
             
         running = True
-        current_watermelon = None
         self.phase = "aim"
-        _pos = [SCREEN_WIDTH / 2 / PPM, SCREEN_HEIGHT / PPM - 2]
         _level = random.randint(0,2)
-
 
         while running: 
             self.world.Step(TIME_STEP, 10, 10)
             self.screen.fill(WHITE)
+            
+            for body in self.world.bodies:
+                for fixture in body.fixtures:
+                    fixture.shape.draw(body, fixture)
 
+            # 동일 레벨의 두 과일이 부딪힌 경우 ->  더 높은 레벨의 과일을 중간 위치에 생성
             while len(self.contact_listener.to_destroy) != 0:
                 _tmp = self.contact_listener.to_destroy[-1]
                 self.contact_listener.to_destroy.pop()
@@ -150,37 +169,41 @@ class WatermelonGame:
                 self.watermelons.append(self.create_watermelon(new_pos, new_level))
                 self.before_positions = [melon.position for melon in self.watermelons]
 
-            for body in self.world.bodies:
-                for fixture in body.fixtures:
-                    fixture.shape.draw(body, fixture)
-
             ### 조준 phase ###
             if self.phase == "aim":
-                pygame.draw.circle(self.screen, MELONS[_level][1], [int(_pos[0] * PPM), int( SCREEN_HEIGHT - (_pos[1] * PPM)) ], int(MELONS[_level][2] * PPM))
+                # 과일 표시
+                pygame.draw.circle(self.screen, MELONS[_level][1], [int(self.cursor[0] * PPM), int( SCREEN_HEIGHT - (self.cursor[1] * PPM)) ], int(MELONS[_level][2] * PPM))
+                # 조준선 표시
+                self.draw_dotted_line((self.cursor[0], self.cursor[1]-35), (self.cursor[0], self.cursor[1]), (255, 181, 0))
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         running = False
                     if event.type == pygame.KEYDOWN:
                         print(event.key)
                         if event.key == pygame.K_LEFT:
-                            _pos[0] -= 0.3
+                            self.change_cursor_position(-0.3)
                         if event.key == pygame.K_RIGHT:
-                            _pos[0] += 0.3
+                            self.change_cursor_position(0.3)
                         if event.key == pygame.K_DOWN:
                             print(len(self.watermelons))
-                            current_watermelon = self.create_watermelon(_pos, _level)
-                            self.watermelons.append(current_watermelon)
+                            self.watermelons.append(self.create_watermelon(self.cursor, _level))
 
-                            # go to drop phase
+                            # Drop phase 로 변경
                             self.phase = "drop"
                             self.before_positions = [melon.position for melon in self.watermelons]
 
+                # 키를 꾹 누르는 경우 인식
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_LEFT]:
+                    self.change_cursor_position(-0.3)
+                if keys[pygame.K_RIGHT]:
+                    self.change_cursor_position(0.3)
+
+            ### Drop phase : 수박이 떨어지는 phase
             elif self.phase == "drop":
                 if self.check_all_melons_stop():
                     self.phase = "aim"
-                    _pos = [SCREEN_WIDTH / 2 / PPM, SCREEN_HEIGHT / PPM - 2]
                     _level = random.randint(0,2)
-                pass
             
             pygame.display.flip()
             self.clock.tick(FPS)
